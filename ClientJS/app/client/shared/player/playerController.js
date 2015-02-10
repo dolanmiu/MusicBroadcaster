ff7bcbe24ab14a30211a9550d1c8ca50908ea003
2khc <kh.kelvinchan@gmail.com>
2015-02-10 00:18:06 +0000

    Updated player controller to try to create a player instance.

--- ClientJS/app/client/shared/player/playerController.js
+++ ClientJS/app/client/shared/player/playerController.js
@@ -1,7 +1,41 @@
 /**
  * Created by Kelv on 09/02/2015.
  */
+/*globals angular, document, done, YT */
 angular.module('app')
-    .controller('playerController', function () {
+    .controller('playerController', function ($scope, playerService, $window) {
+        'use strict';
+        var player;
 
+        $scope.createPlayer = function () {
+            var tag = document.createElement('script'),
+                targetTag = document.getElementById('hello');
+
+            tag.src = "https://www.youtube.com/iframe_api";
+            targetTag.parentNode.insertBefore(tag, targetTag);
+
+            $window.onYouTubeIframeAPIReady = function () {
+                player = new YT.Player('player', {
+                    height: '390',
+                    width: '640',
+                    videoId: videoId,
+                    events: {
+                        'onReady': function (event) {
+                            event.target.playVideo();
+                        },
+                        'onStateChange': function (event) {
+                            if (event.data === YT.PlayerState.PLAYING && !done) {
+                                setTimeout(stopVideo, 6000);
+                                done = true;
+                            }
+                        }
+                    }
+                });
+            };
+
+        };
+
+        $scope.loadNewVideo = function () {
+            player.loadVideoById($scope.videoId, 5, "large");
+        };
     });
