698a9ff6503850e1b40e1b3b76d1c443e4fab52a
2khc <kh.kelvinchan@gmail.com>
2015-02-15 16:49:02 +0000

    Initial commit of restService

--- ClientJS/app/client/components/web.sockets/restService.js
+++ ClientJS/app/client/components/web.sockets/restService.js
@@ -1,3 +1,21 @@
 /**
  * Created by Kelv on 15/02/2015.
  */
+/*globals angular, console */
+angular.module('app').service('restService', function ($http, $q) {
+    'use strict';
+
+    this.createRoom = function (roomName) {
+        var name = 'http://localhost:8080/room/create?name=' + roomName,
+            deferred = $q.defer();
+
+        console.log('createRoom() is running' + name);
+        $http.get(name).then(function (response) {
+            console.log(response);
+            deferred.resolve('Room ' + roomName + ' has been created');
+        }, function (reason) {
+            deferred.reject('Room failed to initialise because: ' + reason);
+        });
+        return deferred.promise;
+    };
+});
