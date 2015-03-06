d693c92ec7f10c590f09d422df977a3e1cd6af5e
Dolan Miu <dolan_miu@hotmail.com>
2015-03-06 19:48:35 +0000

    Seekable stop watch It may be depreciated

---
+++ Server/src/main/java/com/poo/musicbroadcaster/model/timer/SeekableStopWatch.java
@@ -0,0 +1,22 @@
+package com.poo.musicbroadcaster.model.timer;
+
+public class SeekableStopWatch extends SimpleStopWatch {
+	private long seekOffset;
+	
+	public SeekableStopWatch() {
+		this.seekOffset = 0;
+	}
+	
+	@Override
+	public long getTimeRun() {
+		System.out.println("before seek offset: " + super.getTimeRun() + ", seek offset: " + this.seekOffset);
+		return super.getTimeRun() + this.seekOffset;
+	}
+
+	public void seekTo(long seek) {
+		long seekDifference = seek - this.getTimeRun();
+		this.seekOffset += seekDifference;
+	}
+	
+	
+}
